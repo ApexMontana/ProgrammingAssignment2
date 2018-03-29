6e974542cc4a77cb73975c191bdeb0afe909e36e
ApexMontana <hunter.garbacz17@gmail.com>
2018-03-29 16:40:46 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions below due to the following:
+##		1st Function - Create a special "matrix" object that can
+##					   cache its inverse.
+##		2nd Function - Computes the inverse of the special "matrix" 
+##					   returned by makeCacheMatrix above or retrieve 
+##					   the inverse from the cache. 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache 
+## its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setSpecial <- function(inverse) m <<- inverse
+        getSpecial <- function() m
+        list(set = set, get = get,
+             setSpecial = setSpecial,
+             getSpecial = getSpecial)
 }
 
-
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" 
+##	created by makeCacheMatrix above.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getSpecial()
+        if (!is.null(m)) {
+        		message("getting cached data")
+        		return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setSpecial(m)
+        m
 }
